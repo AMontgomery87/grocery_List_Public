66af392604210438cf84290a808e51cbf3b6fa86
Andrew Montgomery <150795252+AMontgomery87@users.noreply.github.com>
2023-11-25 13:33:37 +0700

    Add files via upload
    better functionality with inventory, receiving, available lists, loading/unloading grocery list, etc and looping through a command loop to access each function

--- groceryInventoryApp.py
+++ groceryInventoryApp.py
@@ -1,6 +1,30 @@
-from groceryTool import*
+from groceryInventory import*
+
+whatarewedoing = (input(" available recipes \n grocery List "
+                        "\n future recipes \n receive groceries "
+                        "\n see inventory \n cook \n "))
+
+def what_do():
+    if "available recipes" in whatarewedoing:
+        available_recipe()
+    elif "grocery list" in whatarewedoing:
+        print(listA)
+    elif "future recipe" in whatarewedoing:
+        future_recipe()
+    elif "receive groceries" in whatarewedoing:
+        while listA != listC:
+            receive_groceries()
+    elif "see inventory" in whatarewedoing:
+        print(listB)
+    elif "cook" in whatarewedoing:
+        cook_recipe()
 
 
+while "N" not in whatarewedoing:
+    what_do()
+    whatarewedoing = (input(" available recipes \n grocery List "
+                        "\n future recipes \n receive groceries "
+                        "\n see inventory \n cook \n "))
 
-load_unload_list()
-print(grocery_list)
+#trying to rectify (listA!=listC) solution to receiving while loop
+#needs to have a break command to go back to (whatarewedoing) loop
